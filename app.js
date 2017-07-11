73b409ad042748c2b0ae99bf146ef03541beb847
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-07-11 19:36:28 -0400

    added routes

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ $routeProvider
   controller: "secondController"
 })
 
+.when("/second/:days",{
+  templateUrl: "pages/forecastpage.html",
+  controller: "secondController"
+})
+
 });
 
 // SERVICES
@@ -33,15 +38,25 @@ $scope.$watch('city', function() {
 })
 }]);
 
-myApp.controller('secondController', ['$scope', '$resource', 'cityService',function($scope, $resource, cityService){
+myApp.controller('secondController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams,
+cityService) {
 
   $scope.city = cityService.city;
+
+  $scope.days = $routeParams.days || '2';
 
   $scope.weatherAPI =
   $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=5d291dcae7f9d1a344a6ea02391feef7',
    { method: "JSONP" });
 
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
 
-    console.log($scope.weatherResult)
+  $scope.convertToFarenheit = function(degK) {
+    return Math.round((1.8 * (degK - 273)) + 32);
+  }
+
+  $scope.converToDate = function(dt) {
+
+    return new Date(dt * 1000);
+  }
 }]);
