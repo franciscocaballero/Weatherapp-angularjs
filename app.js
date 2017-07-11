50812ba4c4613d00a0de0351536714d6a320e3d0
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-07-11 17:40:31 -0400

    added api key

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 var myApp = angular.module('myApp',['ngRoute','ngResource' ]);
 
-myApp.controller('mainController' ['$scope','$routeProvider' ,function($scope,$routeProvider){
 
+// ROUTES
+myApp.config(function ($routeProvider) {
 
-
-}]);
-
-myApp.controller('secondController' ['$scope', '$routeProvider',function($scope, $routeProvider){
-
-
-}]);
-
-myApp.config(function($routeProvider){
 $routeProvider
 
 .when("/", {
@@ -25,3 +17,31 @@ $routeProvider
 })
 
 });
+
+// SERVICES
+myApp.service('cityService', function(){
+this.city = 'Washington, D.C';
+
+})
+
+// CONTROLLERS
+myApp.controller('mainController', ['$scope','cityService', function($scope,cityService){
+$scope.city = cityService.city;
+//'Watching for changes in the input text field and updataing once changes have been made'
+$scope.$watch('city', function() {
+  cityService.city = $scope.city;
+})
+}]);
+
+myApp.controller('secondController', ['$scope', '$resource', 'cityService',function($scope, $resource, cityService){
+
+  $scope.city = cityService.city;
+
+  $scope.weatherAPI =
+  $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=5d291dcae7f9d1a344a6ea02391feef7',
+   { method: "JSONP" });
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+
+    console.log($scope.weatherResult)
+}]);
